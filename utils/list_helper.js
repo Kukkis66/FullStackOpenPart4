5e406f0d73c04c578af7798261b32c34f1c5d2ab
Kukkis66 <matti.kukkasjarvi@gmail.com>
2024-02-08 10:05:38 +0200

    tehtävä 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,19 @@
+
+
+
 const dummy = (blogs) => {
     return 1
   }
+
+
+const totalLikes = (blogs) => {
+
+    const likess = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+        console.log(likess)
+   return likess
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
