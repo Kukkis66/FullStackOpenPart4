1ab5564a3d0521c040c4622e61b8f78e947d2701
Kukkis66 <matti.kukkasjarvi@gmail.com>
2024-02-08 13:20:04 +0200

    tehtävä 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,8 +12,36 @@ const totalLikes = (blogs) => {
         console.log(likess)
    return likess
 }
+
+const mostFavourite = (blogs) => {
+    let maxLikes = 0;
+    
+    
+
+    blogs.map((blog) => {
+        const likesFromBlog = blog.likes;
+        maxLikes = Math.max(maxLikes, likesFromBlog)
+        
+    })
+
+   const result = blogs.filter((blog) => {
+       return blog.likes === maxLikes
+   })
+
+
+   const toReturn = {
+    title: result[0].title,
+    author: result[0].author,
+    likes: result[0].likes
+   }
+
+   return toReturn
+   
+}
+
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    mostFavourite
   }
