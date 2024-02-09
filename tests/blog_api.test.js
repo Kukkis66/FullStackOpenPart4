832e889da39ade20eeb53aa15b6e2904538fad31
Kukkis66 <matti.kukkasjarvi@gmail.com>
2024-02-09 12:13:23 +0200

    tehtävä 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -11,11 +11,11 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-test("bloglist is empty", async () => {
-  const response = await api.get("/api/blogs");
+// test("bloglist is empty", async () => {
+//   const response = await api.get("/api/blogs");
 
-  expect(response.body).toHaveLength(0);
-});
+//   expect(response.body).toHaveLength(0);
+// });
 
 test("identifier are id", async () => {
   const response = await api.get("/api/blogs");
@@ -24,6 +24,32 @@ test("identifier are id", async () => {
     expect(blog.id).toBeDefined();
   });
 });
+
+test('a valid note can be added', async () => {
+    const newBlog = {
+        title: "testTitle",
+        author: "testAuthor",
+        url: "www.testUrl.test",
+        likes: 11,
+    }
+  
+    const response = await api.get('/api/Blogs')
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response2 = await api.get('/api/Blogs')
+  
+    const titles = response2.body.map(r => r.title)
+  
+    expect(response2.body).toHaveLength(response.body.length + 1)
+    expect(titles).toContain(
+      'testTitle'
+    )
+  })
 
 afterAll(async () => {
   await mongoose.connection.close();
