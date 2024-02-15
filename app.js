8d387bbf9aaa9e1bedc63a0fef94fe311597eec8
Kukkis66 <matti.kukkasjarvi@gmail.com>
2024-02-15 13:02:58 +0200

    tehtävä 4.15

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogRouter = require('./controllers/blogs')
-
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
-
-
-
-
-
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+    logger.error("error connection to MongoDB:", error.message);
+  });
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+app.use("/api/users", usersRouter);
+app.use("/api/blogs", blogRouter);
 
-app.use('/api/blogs', blogRouter)
-
-module.exports = app
-
-
-
+module.exports = app;
